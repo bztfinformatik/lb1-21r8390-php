6d8bee0515abf6c7e787699b2f5ae28e959752ac
21r8390 <21r8390@bztf.ch>
2022-11-06 09:04:53 +0100

    📚: DaisyUI

--- app/app/core/Controller.php
+++ app/app/core/Controller.php
@@ -3,60 +3,142 @@ require_once '/var/composer/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\SocketHandler;
+use \Twig\Environment;
+use \Twig\Loader\FilesystemLoader;
 
 class Controller
 {
-    protected $twig;
-    protected $logger;
+    private $twig;
+    private $logger;
 
     public function __construct()
     {
-        $this->loadTwig();
-        $this->loadLogger();
+        $this->initLogger();
+        $this->initTwig();
     }
 
-    protected function model($model)
+    /**
+     * Initialize the logger
+     * 
+     * If `IS_LOGGING` is set to true, the logger will send the logs to the ELK stack
+     */
+    private function initLogger()
     {
+        // Create the logger
+        $this->logger = new Logger('php-controller');
+
+        // Now add some handlers
+        if (IS_LOGGING) {
+            // "logstash" is a host defined by docker-compose
+            $handler = new SocketHandler(LOGSTASH, Logger::DEBUG);
+            $this->logger->pushHandler($handler);
+        }
+
+        $this->log('The logger has been initialized', Logger::NOTICE);
+    }
+
+    /**
+     * Initialize the twig template engine
+     * 
+     * The templates are located in the `app/views` folder
+     */
+    private function initTwig()
+    {
+        // Specify our Twig templates location
+        $loader = new FilesystemLoader(__DIR__ . '/../views');
+
+        // Instantiate our Twig
+        $this->twig = new Environment($loader, [
+            'debug' => true,
+            'cache' => false,
+            'autoescape' => 'html',
+        ]);
+
+        $this->log('The Twig has been initialized', Logger::NOTICE);
+    }
+
+    /**
+     * Render a view using Twig
+     *
+     * @param string $view The view to render
+     * @param array $params The parameters to pass to the view
+     */
+    protected function render($path = '', $data = [])
+    {
+        $this->log('Rendering view: ' . $path, Logger::INFO);
+
+        // Adding default data
+        $path = $path . '.twig.html';
+
+        $data['urlroot'] = URLROOT;
+
+        // Render our view
+        echo $this->twig->render($path, $data);
+    }
+
+    /**
+     * Loads a model from the models folder (`app/models`) and returns it
+     *
+     * @param string $model The model to load
+     * @return BaseModel The model that was loaded
+     */
+    protected function loadModel(string $model)
+    {
+        // Check if the model exists
         if (file_exists('../app/models/' . $model . '.php')) {
+            // Load the model
+            $this->log('Loading the model: ' . $model, Logger::INFO);
             require_once '../app/models/' . $model . '.php';
+            // Instantiate the model
             return new $model();
         } else {
-            echo 'Error : Model does not exists!';
+            $this->log('Model ' . $model . ' does not exists!', Logger::CRITICAL);
         }
     }
 
-    private function loadTwig()
+    /**
+     * Log a message using the logger
+     *
+     * @param string $message The message to log
+     * @param Logger $level The level of the message (default: DEBUG)
+     */
+    public function log(string $message, $level = Logger::DEBUG)
     {
-        // Specify our Twig templates location
-        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../views');
-
-
-        $this->twig = new Twig_Environment($loader, array('debug' => true));
-        $this->twig->addExtension(new Twig_Extension_Debug());
-
-        // Instantiate our Twig - Production, without Debugging
-        // $this->twig = new Twig_Environment($loader);
-    }
-
-    private function loadLogger()
-    {
-        $this->logger = new Logger('elk');
-
-        // "logstash" is a host defined by docker-compose
-        $handler = new SocketHandler(LOGSTASH, Logger::DEBUG);
-        $this->logger->pushHandler($handler);
-
         $visitDetails = [
+            'user_id' => $_SESSION['user_id'] ?? 'Unknown user_id',
+            'user_email' => $_SESSION['user_email'] ?? 'Unknown user_email',
             'ip' => $_SERVER['REMOTE_ADDR'],
             'method' => $_SERVER['REQUEST_METHOD'],
             'uri' => $_SERVER['REQUEST_URI'],
             'agent' => $_SERVER['HTTP_USER_AGENT'],
             'referer' => $_SERVER['HTTP_REFERER'] ?? 'not set'
         ];
-        $this->logger->info("Example of a request being logged", $visitDetails);
 
-        // $this->logger = new MonologLogger('channel-name');
-        // $this->logger->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::DEBUG));
-        // $this->logger->info('My logger is now ready');
+        switch ($level) {
+            case Logger::INFO:
+                $this->logger->info($message, $visitDetails);
+                break;
+            case Logger::NOTICE:
+                $this->logger->notice($message, $visitDetails);
+                break;
+            case Logger::WARNING:
+                $this->logger->warning($message, $visitDetails);
+                break;
+            case Logger::ERROR:
+                $this->logger->error($message, $visitDetails);
+                break;
+            case Logger::CRITICAL:
+                $this->logger->critical($message, $visitDetails);
+                break;
+            case Logger::ALERT:
+                $this->logger->alert($message, $visitDetails);
+                break;
+            case Logger::EMERGENCY:
+                $this->logger->emergency($message, $visitDetails);
+                break;
+            default:
+                $this->logger->debug($message, $visitDetails);
+                break;
+        }
     }
 }
